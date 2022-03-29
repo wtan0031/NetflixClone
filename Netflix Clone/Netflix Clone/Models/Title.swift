d3628b398d59b72a5746d90f0d05b31c31692829
Wei Liang <wtan0031@gmail.com>
2022-03-29 21:58:36 +0800

    Remove comment

--- Netflix Clone/Netflix Clone/Models/Title.swift
+++ Netflix Clone/Netflix Clone/Models/Title.swift
@@ -27,26 +27,3 @@ struct Title: Codable {
     let vote_average: Double
     
 }
-
-/*
- adult = 0;
- "backdrop_path" = "/2rNt11UPFYpz0MXMephmO0uyM5h.jpg";
- "genre_ids" =             (
-     878,
-     12,
-     35
- );
- id = 696806;
- "media_type" = movie;
- "original_language" = en;
- "original_title" = "The Adam Project";
- overview = "After accidentally crash-landing in 2022, time-traveling fighter pilot Adam Reed teams up with his 12-year-old self on a mission to save the future.";
- popularity = "283.934";
- "poster_path" = "/sI3hQMTyrF9F8jU1qA8qPHLKCgG.jpg";
- "release_date" = "2022-03-11";
- title = "The Adam Project";
- video = 0;
- "vote_average" = "7.1";
- "vote_count" = 74;
-},
- */
